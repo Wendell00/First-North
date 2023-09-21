6ea94134bac74e429cc95f3b73c27cb582e3d956
wendell00 <wendellborges2003@gmail.com>
2023-09-20 21:05:55 -0300

    add aside

--- src/index.js
+++ src/index.js
@@ -14,6 +14,36 @@ window.addEventListener('scroll', function(){
   }
 });
 
+aside = document.querySelector('.aside-nav')
+menuIcon = document.querySelector('.menu-icon')
+closeIcon = document.querySelector('.aside-x')
+
+menuIcon.addEventListener('click', () => {
+    aside.classList.toggle('display-toggle');
+    body.classList.toggle('overflow-toggle')
+});
+
+closeIcon.addEventListener('click', () => {
+    aside.classList.toggle('display-toggle');
+    body.classList.toggle('overflow-toggle')
+});
+
+window.addEventListener('scroll', function(){
+  const navbar = document.querySelector('.navbar');
+  const navinfo = document.querySelector('.nav-info-container')
+  const scrollPosition = window.scrollY;
+
+  if (window.innerWidth > 768) {
+    if (scrollPosition >= 40) {
+      navbar.classList.add('fixed');
+      navinfo.classList.add('scroll-fixed')
+    } else {
+      navbar.classList.remove('fixed');
+      navinfo.classList.remove('scroll-fixed')
+    }
+  }
+});
+
 // var dataAtual = new Date();
 
 // // Obtém o ano atual
